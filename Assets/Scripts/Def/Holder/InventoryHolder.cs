6d15b788cc21344b0c8a160a4744388cf472784b
LubieKakao1212 <grzesio.go@gmail.com>
2022-03-24 12:50:11 +0100

    Implemented InventoryHolders Implemented InventoryPopulators Implemented ItemStackDefinition and its PropertyDrawer

---
+++ Assets/Scripts/Def/Holder/InventoryHolder.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Inventory.Definition
+{
+    using Api;
+    using Inv;
+
+    public class InventoryHolder : MonoBehaviour
+    {
+        public IInventory Inventory => inventory;
+
+        [SerializeField]
+        protected int inventorySize;
+        
+        private IInventory inventory;
+
+        private void Awake()
+        {
+            this.inventory = CreateInventory();
+        }
+
+        protected virtual IInventory CreateInventory()
+        {
+            return new Inventory(inventorySize);
+        }
+    }
+}
---
+++ Assets/Scripts/Def/Populator/SimpleInventoryPopulator.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Inventory.Definition
+{
+    using Api;
+
+    public class SimpleInventoryPopulator : InventoryPopulator
+    {
+        [SerializeField]
+        private ItemStackDefinition[] insertedContent;
+
+        public override void Populate(IInventory inv)
+        { 
+            base.Populate(inv);
+
+            foreach (var stack in insertedContent)
+            {
+                inv.Insert(stack.ItemStack);
+            }
+        }
+    }
+}
