a19ac316065a118910ac28a49f0e8db4538faa3a
LubieKakao1212 <grzesio.go@gmail.com>
2022-03-25 09:37:45 +0100

    Implemented ProviderInventoryHolder Added some more documentation

---
+++ Assets/Scripts/Def/Holder/ProviderInventoryHolder.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Inventory.Definition
+{
+    using Api;
+    using Inv;
+    using Items;
+
+    public class ProviderInventoryHolder : InventoryHolder
+    {
+        [SerializeField]
+        private ItemStackDefinition[] content;
+
+        protected override IInventory CreateInventory()
+        {
+            IInventory inv = new ProviderInventory(content.Length);
+            for (int i = 0; i < content.Length; i++) 
+            {
+                inv[i] = content[i].ItemStack;
+            }
+            return inv;
+        }
+
+    }
+}
--- Assets/Scripts/Def/ItemStackDefinition.cs
+++ Assets/Scripts/Def/ItemStackDefinition.cs
@@ -5,10 +5,13 @@ namespace Inventory.Definition
 {
     using Items;
 
+    /// <summary>
+    /// Helper class for representing ItemStacks in the editor
+    /// </summary>
     [Serializable]
     public class ItemStackDefinition
     {
-        public ItemStack ItemStack => new ItemStack(item.Item, amount);
+        public ItemStack ItemStack => item != null ? new ItemStack(item.Item, amount) : ItemStack.Empty;
         
         [SerializeField]
         private int amount = 1;
