a9841200f5c6cbbd9b23930cd9ebf408373bd231
LubieKakao1212 <grzesio.go@gmail.com>
2022-03-25 08:48:58 +0100

    Item drag is done

---
+++ Assets/Scripts/UI/SlotPickUpHandler.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Inventory.UI
+{
+    using Definition;
+    using Inv;
+    using Api;
+    using static Inv.InventoryUtil;
+
+    [RequireComponent(typeof(Slot))]
+    public class SlotPickUpHandler : MonoBehaviour, IPointerDownHandler
+    {
+        private Slot slot;
+
+        private void Start()
+        {
+            slot = GetComponent<Slot>();
+        }
+
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            if (enabled)
+            {
+                TrySwapSlotContents(slot.Parent.Inventory, slot.SlotIndex, GlobalInventories.Instance.PickedUp, 0);
+            }
+        }
+    }
+}
